5b3661f79feeb7d59382a69d4243000645b05745
melisayunitasari50-cmyk <melisayunitasari50@gmail.com>
2026-05-27 02:11:15 +0700

    Selesaikan modul karyawan: Controller, Model, Migrasi, dan View

---
+++ app/Http/Controllers/KaryawanController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Karyawan;
+
+class KaryawanController extends Controller
+{
+    public function index()
+    {
+        $karyawan = Karyawan::all();
+        return view('karyawan.index', compact('karyawan'));
+    }
+
+    public function create()
+    {
+        return view('karyawan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'jabatan' => 'required',
+        ]);
+
+        Karyawan::create($request->all());
+        return redirect()->route('karyawan.index')->with('success', 'Data berhasil disimpan!');
+    }
+
+    public function edit($id)
+    {
+        $karyawan = Karyawan::findOrFail($id);
+        return view('karyawan.edit', compact('karyawan'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $karyawan = Karyawan::findOrFail($id);
+        $karyawan->update($request->all());
+        return redirect()->route('karyawan.index')->with('success', 'Data berhasil diupdate!');
+    }
+
+    public function destroy($id)
+    {
+        $karyawan = Karyawan::findOrFail($id);
+        $karyawan->delete();
+        return redirect()->route('karyawan.index')->with('success', 'Data berhasil dihapus!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,42 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MejaController;
 use App\Http\Controllers\MenuController;
-<<<<<<< HEAD
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PelangganRestoranController; // Pastikan ini ada
+use App\Http\Controllers\KaryawanController;         // Pastikan ini ada
 
-// 1. Halaman Utama Website
-=======
-use App\Http\Controllers\PelangganRestoranController;
-
-// Halaman utama sekarang mengarah ke Dashboard Restoran
->>>>>>> 8cc7fbf0915b5fc3028a93d0e1f2396c41d82ef4
+// 1. Halaman Utama
 Route::get('/', function () {
     return view('dashboard');
 });
 
-<<<<<<< HEAD
-// 2. Jalur Modul Pelanggan (Punya Kawanmu)
-Route::get('/pelanggan', [PelangganRestoranController::class, 'index'])->name('pelanggan.index');
-Route::get('/pelanggan/tambah', [PelangganRestoranController::class, 'create'])->name('pelanggan.create');
-Route::post('/pelanggan/simpan', [PelangganRestoranController::class, 'store'])->name('pelanggan.store');
-Route::get('/pelanggan/{id}/edit', [PelangganRestoranController::class, 'edit'])->name('pelanggan.edit');
-Route::put('/pelanggan/{id}', [PelangganRestoranController::class, 'update'])->name('pelanggan.update');
-Route::delete('/pelanggan/{id}', [PelangganRestoranController::class, 'destroy'])->name('pelanggan.destroy');
-
-// 3. Jalur Modul Menu (Punya Pesi)
-Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
-Route::get('/menu/tambah', [MenuController::class, 'create'])->name('menu.create');
-Route::post('/menu/simpan', [MenuController::class, 'store'])->name('menu.store');
-
-// 4. Jalur Modul Pesanan Makanan (Punya Pesi - Tanpa Edit & Hapus)
-Route::get('/order', [OrderController::class, 'index'])->name('order.index');
-Route::get('/order/tambah', [OrderController::class, 'create'])->name('order.create');
-Route::post('/order/simpan', [OrderController::class, 'store'])->name('order.store');
-=======
-// Route untuk Modul-Modul (Meja dan Menu)
+// 2. Modul Meja
 Route::resource('meja', MejaController::class);
+
+// 3. Modul Menu
 Route::resource('menu', MenuController::class);
 
-// Jalur untuk pelanggan
+// 4. Modul Pelanggan
 Route::resource('pelanggan', PelangganRestoranController::class);
->>>>>>> 8cc7fbf0915b5fc3028a93d0e1f2396c41d82ef4
+
+// 5. Modul Pesanan
+Route::resource('order', OrderController::class);
+
+// 6. Modul Karyawan
+Route::resource('karyawan', KaryawanController::class);
